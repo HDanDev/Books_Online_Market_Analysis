6c0016eac4cf66d5607f2aaaa1bf6a36c8d246f3
HDanDev <dhan_nayphotmail.com>
2023-07-13 17:40:19 +0200

    switch to the request_html library and dictionary use + multiple data fetch

--- Books_Online_Market_Analysis/Books_Online_Market_Analysis.py
+++ Books_Online_Market_Analysis/Books_Online_Market_Analysis.py
@@ -1,24 +1,30 @@
 
 import csv
-import requests
-from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
+session = HTMLSession()
 url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-response = requests.get(url)
+response = session.get(url)
 
-soup = BeautifulSoup(response.content, 'html.parser')
+filename = 'scrappedData.csv'
+scrappedProduct = {
+    'product_page_url': 'null',
+    'universal_ product_code (upc)': 'null',
+    'title': 'null',
+    'price_including_tax': 'null',
+    'price_excluding_tax': 'null',
+    'number_available': 'null',
+    'product_description': 'null',
+    'category': 'null',
+    'review_rating': 'null',
+    'image_url': 'null',
+}
 
-data = []
-
-paragraphs = soup.find_all('h1')
-for paragraph in paragraphs:
-    data.append(paragraph.get_text())
-
-    column_titles = ['Title']
-    filename = 'scrapeddata.csv'
+scrappedProduct['product_page_url'] = url
+scrappedProduct['title'] = response.html.find('title', first = True)
+   
 with open(filename, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(column_titles)
-    for item in data:
-        writer.writerow([item])
-
+    #print(scrappedProduct.values())
+    writer = csv.DictWriter(csvfile, scrappedProduct.keys())
+    writer.writeheader()
+    writer.writerow(scrappedProduct)
