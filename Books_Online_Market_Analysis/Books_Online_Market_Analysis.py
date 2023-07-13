04ef114bd05de5708ece40429a5d8af0b5db4a48
HDanDev <dhan_nayphotmail.com>
2023-07-13 15:18:38 +0200

    Base scrapping functionality

--- Books_Online_Market_Analysis/Books_Online_Market_Analysis.py
+++ Books_Online_Market_Analysis/Books_Online_Market_Analysis.py
@@ -1,1 +1,24 @@
 
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+data = []
+
+paragraphs = soup.find_all('h1')
+for paragraph in paragraphs:
+    data.append(paragraph.get_text())
+
+    column_titles = ['Title']
+    filename = 'scrapeddata.csv'
+with open(filename, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(column_titles)
+    for item in data:
+        writer.writerow([item])
+
