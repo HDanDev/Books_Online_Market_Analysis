689e28a4f82179aa83ae4a7701decdfce968f925
HDanDev <dhan_nayphotmail.com>
2023-07-15 17:31:30 +0200

    All infos basic scraping for one product done

--- Books_Online_Market_Analysis/Books_Online_Market_Analysis.py
+++ Books_Online_Market_Analysis/Books_Online_Market_Analysis.py
@@ -1,5 +1,6 @@
 
 import csv
+from http.client import responses
 from requests_html import HTMLSession
 
 session = HTMLSession()
@@ -21,10 +22,18 @@ scrappedProduct = {
 }
 
 scrappedProduct['product_page_url'] = url
-scrappedProduct['title'] = response.html.find('title', first = True)
+scrappedProduct['universal_ product_code (upc)'] = response.html.find('table > tr > td' , first = True).text
+scrappedProduct['title'] = response.html.find('div.col-sm-6.product_main > h1', first = True).text
+scrappedProduct['price_including_tax'] = response.html.find('table' , first = True).find('tr > td')[3].text
+scrappedProduct['price_excluding_tax'] = response.html.find('table' , first = True).find('tr > td')[2].text
+scrappedProduct['number_available'] = response.html.search('In stock ({} available)')[0]
+scrappedProduct['product_description'] = response.html.find('article.product_page > p', first = True).text
+scrappedProduct['category'] = response.html.find('ul.breadcrumb' , first = True).find('li > a')[2].text
+scrappedProduct['review_rating'] = response.html.search('"star-rating {}"')[0]
+scrappedProduct['image_url'] = response.html.find('[src]', first = True).attrs['src']
+#print(response.text)
    
 with open(filename, 'w', newline='') as csvfile:
-    #print(scrappedProduct.values())
     writer = csv.DictWriter(csvfile, scrappedProduct.keys())
     writer.writeheader()
     writer.writerow(scrappedProduct)
